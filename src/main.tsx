b811b7fef5ed2284249fc6f63b1bee6394c96ddf
Mitch J <mjacovet@kent.edu>
2025-03-01 01:15:42 -0500

    use vite-react-ssg for build

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App.mdx";
+import { ViteReactSSG } from "vite-react-ssg/single-page";
 import "@/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const app = (
   <React.StrictMode>
     <div className="max-w-prose mx-auto">
       <App />
     </div>
   </React.StrictMode>
 );
+
+if (typeof window !== "undefined") {
+  ReactDOM.createRoot(document.getElementById("root")!).render(app);
+}
+
+export const createRoot = ViteReactSSG(app);
